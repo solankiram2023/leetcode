aefb2658affd2566a0fa23479c8c72b8ad3e9927
Ramy Solanki <145072212+solankiram2023@users.noreply.github.com>
2025-04-06 12:59:45 -0400

    Time: 6 ms (47.14%), Space: 59.8 MB (69.13%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,3 +1,7 @@
+
+
+// import com.sun.org.apache.xalan.internal.xsltc.dom.CurrentNodeListFilter;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -8,131 +12,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return false;
 
- /*
-Approach:
-1. This solution converts the linked list into an ArrayList to use index-based access.
-2. Then, we apply the two pointer technique to check whether the list is a palindrome.
-3. If values from the front and back match all the way through, its a palindrome
-4. This approach trades off space for simplicity (O(n) space)
-
-
- */
-
-
- /*
-Steps:
-1. Create an Arraylist to store the values of the LinkedList
-2. Traverse the linked list and store each node's value in the list
-3. Use two-pointer approach to compare front and back
-4. Important use .equals() for comparing Integer objects
-5. If Mismatch is found -> not a palindrome
-6. All matched -> the linked list is a palindrome
-
- */
-
-// class Solution {
-//     public boolean isPalindrome(ListNode head) {
-//         List<Integer> vals = new ArrayList<>();
-//         ListNode currentNode = head;
-
-//         while (currentNode != null) {
-//             vals.add(currentNode.val);
-//             currentNode = currentNode.next;
-//         }
-
-//         int front = 0;
-//         int back = vals.size() -1;
-
-//         while(front<back){
-
-//             if(!vals.get(front).equals(vals.get(back))) {
-//                 return false;
-//             }
-
-//             front++;
-//             back--;
-//         }
-
-//         return true;
-        
-//     }
-// }
-
-/*
-Time Complexity:O(n)
-O(n) to traverse the linked list and store values in an ArrayList
-O(n) to compare values using the two-pointer technique.
-Total = O(n)
-
-Space Complexity: O(n):
-O(n) additional space is used to store node values in ArrayList.
-No modifications are made to the original linkedlist.
-
-*/
-
-/*
-Approach #2:
-
--This solution uses a recursive approach to compare nodes from front and back simultaneously.
--It uses a class level pointer 'frontPointer' that starts from head and moves forward.
--The recursion drives to the end of the list (backward traversal), and as the recursion stack unwinds,
-it compares the current node (from back) with the 'frontPointer' (from front).
--If all values match during unwinding, the list is a palindrome.
-
--This approach is elegant and avoids modifying the list or using extra data structures like ArrayList.
-
-*/
-
-/*
-Steps:
-1. Class-level pointer that moves forward during the recursion unwinding
-2. Recursive function to check if the list is a palindrome
-3. Base case: If we have reached the end of the list , return true
-4. Recursive call to go deeper into the list
-5. If a missmatch has already occured, propagate false
-6. Check if the current node value matches frontPointer's value
-7. Values don't match -> not a palindrome
-8. Move frontPointer forward for the next comparison during unwinding
-9. All matched-> its palindrome
-10. Public method that initializes the front pointer and starts recursion
-
-*/
-
-class Solution{
-    private ListNode frontPointer;
-
-    private boolean recursivelyCheck(ListNode currentNode){
-
-        if (currentNode != null){
-
-            if(!recursivelyCheck(currentNode.next)){
-                return false;
+        ListNode firstHalfEnd=endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result=true;
+        while(result && p2!=null){
+            if(p1.val != p2.val){
+                result =false;
             }
-
-            if (currentNode.val != frontPointer.val){
-                return false;
-            }
-
-            frontPointer = frontPointer.next;
-
+            p1=p1.next;
+            p2=p2.next;
         }
-
-        return true;
+        return result;
     }
 
-    public boolean isPalindrome(ListNode head){
-        frontPointer = head;
-        return recursivelyCheck(head);
+
+    private ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr!=null){
+            ListNode nextTemp = curr.next;
+            curr.next =prev;
+            prev=curr;
+            curr=nextTemp;
+        }
+        return prev;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head){
+        ListNode fast =head;
+        ListNode slow = head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
-
-/*
-Time Complexity: O(n)
--Each node is visited exactly once during the recursion (depth = n)
--Total time is linear in the number of nodes.
-
-Space Complexity: O(n)
--Due to recursion the call stack grows up to the length of the list (n).
--No additional data structure are used.
-*/
